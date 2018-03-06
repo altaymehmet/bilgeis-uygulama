03f7508cc84c0e2a2ae351c33ee186e53287ddc7
altaymehmet <36675175+altaymehmet@users.noreply.github.com>
2018-03-06 21:18:43 +0300

    Add files via upload

---
+++ kosul_deyimi.py
@@ -0,0 +1,12 @@
+yas=int(input("yaşınızı giriniz..."))
+
+if yas<100:
+
+    if yas <50:
+        print("0-50 aralığında bir değer girdiniz..")
+
+    elif yas>50:
+        print("50-100 arasında değer girdiniz..")
+
+else:
+    print("0-100 arasında bir değer giriniz..")
